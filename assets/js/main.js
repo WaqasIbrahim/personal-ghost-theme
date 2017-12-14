7cfd9ebf96297779d39f9af5e8706d9dafe8f667
Waqas Ibrahim <waqas.abrahim@gmail.com>
2017-12-14 09:33:13 +0500

    refactored and fixed eslint errors

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-(function(){
+(function() {
 
 	'use strict';
 
@@ -9,27 +9,27 @@
 
 	navToggle.addEventListener('click', function(e) {
 		e.preventDefault();
-		this.classList.toggle('active');
 
+		this.classList.toggle('active');
 		body.classList.toggle('nav-open');
 	}, false);
 
-	
 	// Sharing Links
 	var socialLinks = document.getElementsByClassName('social-share');
 
 	for(var i = 0, len = socialLinks.length; i < len; i++) {
 		socialLinks[i].addEventListener('click', function(e) {
 			e.preventDefault();
+
 			popup(this.href);
 		}, false);
 	}
 
 	function popup(url) {
-		var left = (screen.width - 570) / 2,
-		top = (screen.height - 570) / 2,
-		params = 'menubar=no,toolbar=no,status=no,width=570,height=570,top=' + top + ',left=' + left;
-		
+		var left = (screen.width - 570) / 2;
+		var top = (screen.height - 570) / 2;
+		var params = 'menubar=no,toolbar=no,status=no,width=570,height=570,top=' + top + ',left=' + left;
+
 		window.open(url, '_blank', params);
 	}
 
